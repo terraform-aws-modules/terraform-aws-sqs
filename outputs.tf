8e46b2e2368bfd51ccf41949ea41e653b0ce2dbb
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-27 14:27:16 -0400

    fix: Correct linting failures

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,14 @@
 output "sqs_queue_id" {
   description = "The URL for the created Amazon SQS queue"
-  value = element(
-    concat(
-      aws_sqs_queue.this.*.id,
-      [""],
-    ),
-    0,
-  )
+  value       = try(aws_sqs_queue.this[0].id, "")
 }
 
 output "sqs_queue_arn" {
   description = "The ARN of the SQS queue"
-  value = element(
-    concat(
-      aws_sqs_queue.this.*.arn,
-      [""],
-    ),
-    0,
-  )
+  value       = try(aws_sqs_queue.this[0].arn, "")
 }
 
 output "sqs_queue_name" {
   description = "The name of the SQS queue"
-  value = element(
-    concat(
-      data.aws_arn.this.*.resource,
-      [""],
-    ),
-    0,
-  )
+  value       = try(data.aws_arn.this[0].resource, "")
 }
