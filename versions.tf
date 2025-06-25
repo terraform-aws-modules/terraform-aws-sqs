3a02cbef510ecf74bb68e26210254db094ddfa96
iodeslykos <42@iodeslykos.com>
2025-06-25 17:30:20 -0400

    fix(versions): TF v1.10.0, AWS v6.0

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.36"
+      version = ">= 6.0"
     }
   }
 }
